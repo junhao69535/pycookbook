7c4eed23304cea569b2da62ed3913f0f6489e712
Zhao Haobin <zhaohaobin@qianxin.com>
2019-09-10 19:16:17 +0800

    add content into 1.2.py

--- chapter1/1.2.py
+++ chapter1/1.2.py
@@ -1,2 +1,13 @@
 #!coding=utf-8
-print 'test1.2'
+
+"""
+    解压可迭代对象赋值给多个变量
+"""
+
+# 如果只想获取可迭代对象的某几个元素，可是可迭代对象中的元素过多，此时可以使用'*'
+# python2不支持这种语法
+# def drop_first_last(grades):
+#     first, *middle, last = grades
+#     return avg(middle)
+
+
